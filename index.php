9340562267d877b7b26301a3fef16ee73ef4a45a
ahmadasnawi <ahmadaries899@gmail.com>
2024-08-04 09:25:42 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,348 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <section>
+        <div class="left-side">
+            <h1><span>P</span>J</h1>
+            <div class="icon">
+                <i class="fa-solid fa-house"></i>
+                <p>Home</p>
+            </div>
+            <div class="icon">
+                <i class="fa-regular fa-gem"></i>
+                <p>Products</p>
+            </div>
+            <div class="icon">
+                <i class="fa-regular fa-circle-user"></i>
+                <p>Customers</p>
+            </div>
+            <div class="icon">
+                <i class="fa-solid fa-store"></i>
+                <p>shop</p>
+            </div>
+            <div class="icon">
+                <i class="fa-solid fa-percent"></i>
+                <p>Promote</p>
+            </div>
+            <div class="icon icon-a">
+                <i class="fa-regular fa-circle-question"></i>
+                <p>Help</p>
+            </div>
+        </div>
+        <div class="right-side">
+            <nav>
+                <div class="left">
+                    <div></div>
+                    <p>Products</p>
+                    <p>Selected</p>
+                </div>
+                <div class="right">
+                    <button>
+                        <i class="fa-solid fa-plus"></i>
+                        <p>Order</p>
+                    </button>
+                    <i class="fa-regular fa-message"></i>
+                    <i class="fa-regular fa-bell"></i>
+                    <div class="border-radius">
+                        <i class="fa-solid fa-user"></i>
+                    </div>
+                </div>
+            </nav>
+            <header>
+                <div class="top-line">
+                    <div class="container-1">
+                        <div class="container-a">
+                            <h1>Explore New</h1>
+                            <div class="box">
+                                <h2>Air Spain</h2>
+                                <p><span>$149.00</span> <s>$199,00</s> </p>
+                                <div class="colors">
+                                    <button>Colors</button>
+                                    <div class="radius">
+                                        <div class="radius-x radius1"></div>
+                                        <div class="radius-x radius2"></div>
+                                        <div class="radius-x radius3"></div>
+                                        <div class="radius-x radius4"></div>
+                                        <div class="radius-x radius5"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="produk">
+                                <div class="ket-1">NEW</div>
+                                <div class="ket-2">HOT</div>
+                            </div>
+                        </div>
+                        <div class="container-b">
+                            <img src="./img/shoes-pelangi.png" alt="">
+                            <h1>10K+</h1>
+                        </div>
+                    </div>
+                    <div class="container-2">
+                        <div class="top">
+                            <div class="ket-table">
+                                <div class="garis"></div>
+                                <div class="views">Product Views</div>
+                            </div>
+                            <select name="" id="">
+                                <option value="">Last 7 days</option>
+                                <option value="">Last 8 days</option>
+                                <option value="">Last 9 days</option>
+                            </select>
+                        </div>
+                        <div class="mid">
+                            <div class="kiri">
+                                <li>30</li>
+                                <li>25</li>
+                                <li>20</li>
+                                <li>15</li>
+                                <li>10</li>
+                                <li>5</li>
+                            </div>
+                            <div class="kanan">
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                            </div>
+                        </div>
+                        <div class="bottom">
+                            <li>22</li>
+                            <li>23</li>
+                            <li>24</li>
+                            <li>25</li>
+                            <li>26</li>
+                            <li>27</li>
+                            <li>28</li>
+                        </div>
+                    </div>
+                </div>
+                <div class="bottom-line">
+                    <div class="right-right">
+                        <div class="line-atas">
+                            <div class="kanan">
+                                <div class="garis"></div>
+                                <p>Recent Products</p>
+                            </div>
+                            <div class="kiri">
+                                <select name="" id="">
+                                    <option value="">Last 7 days</option>
+                                    <option value="">Last 8 days</option>
+                                    <option value="">Last 9 days</option>
+                                </select>
+                                <button>New Post</button>
+                            </div>
+                        </div>
+                        <div class="line-bawah">
+                            <div class="product">
+                                <div class="product-1">
+                                    <div class="box">
+                                        <img src="./img/shoes-white.png" alt="">
+                                    </div>
+                                    <h1>Nike Air</h1>
+                                    <div class="harga">
+                                        <p>$180.00</p>
+                                        <p><s>$289,00</s></p>
+                                    </div>
+                                    <div class="color">
+                                        <button>Colors</button>
+                                        <div class="radius">
+                                            <div class="radius-x radius-a"></div>
+                                            <div class="radius-x radius-b"></div>
+                                            <div class="radius-x radius-c"></div>
+                                            <div class="radius-x radius-d"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="product-2">
+                                    <div class="box">
+                                        <div class="judul">NEW</div>
+                                        <img src="./img/shoes-pink.png" alt="">
+                                    </div>
+                                    <h1>Nike Jordan</h1>
+                                    <div class="harga">
+                                        <p>$199.00</p>
+                                        <p><s>$289,00</s></p>
+                                    </div>
+                                    <div class="color">
+                                        <button>Colors</button>
+                                        <div class="radius">
+                                            <div class="radius-x radius-a"></div>
+                                            <div class="radius-x radius-b"></div>
+                                            <div class="radius-x radius-c"></div>
+                                            <div class="radius-x radius-d"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="product-3">
+                                    <div class="box">
+                                        <div class="judul">NEW</div>
+                                        <div class="judul-1">HOT</div>
+                                        <img src="./img/shoes-tosca.png" alt="">
+                                    </div>
+                                    <h1>Nike Netro</h1>
+                                    <div class="harga">
+                                        <p>$135.00</p>
+                                        <p><s>$289,00</s></p>
+                                    </div>
+                                    <div class="color">
+                                        <button>Colors</button>
+                                        <div class="radius">
+                                            <div class="radius-x radius-a"></div>
+                                            <div class="radius-x radius-b"></div>
+                                            <div class="radius-x radius-c"></div>
+                                            <div class="radius-x radius-d"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="product-4">
+                                    <div class="box">
+                                        <img src="./img/shoes-pelangi.png" alt="">
+                                    </div>
+                                    <h1>Nike Spain</h1>
+                                    <div class="harga">
+                                        <p>$149.00</p>
+                                        <p><s>$199,00</s></p>
+                                    </div>
+                                    <div class="color">
+                                        <button>Colors</button>
+                                        <div class="radius">
+                                            <div class="radius-x radius-a"></div>
+                                            <div class="radius-x radius-b"></div>
+                                            <div class="radius-x radius-c"></div>
+                                            <div class="radius-x radius-d"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="product-1">
+                                    <div class="box">
+                                        <img src="./img/shoes-white.png" alt="">
+                                    </div>
+                                    <h1>Nike Air</h1>
+                                    <div class="harga">
+                                        <p>$180.00</p>
+                                        <p><s>$289,00</s></p>
+                                    </div>
+                                    <div class="color">
+                                        <button>Colors</button>
+                                        <div class="radius">
+                                            <div class="radius-x radius-a"></div>
+                                            <div class="radius-x radius-b"></div>
+                                            <div class="radius-x radius-c"></div>
+                                            <div class="radius-x radius-d"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="product-2">
+                                    <div class="box">
+                                        <div class="judul">NEW</div>
+                                        <img src="./img/shoes-pink.png" alt="">
+                                    </div>
+                                    <h1>Nike Jordan</h1>
+                                    <div class="harga">
+                                        <p>$199.00</p>
+                                        <p><s>$289,00</s></p>
+                                    </div>
+                                    <div class="color">
+                                        <button>Colors</button>
+                                        <div class="radius">
+                                            <div class="radius-x radius-a"></div>
+                                            <div class="radius-x radius-b"></div>
+                                            <div class="radius-x radius-c"></div>
+                                            <div class="radius-x radius-d"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="product-3">
+                                    <div class="box">
+                                        <div class="judul">NEW</div>
+                                        <div class="judul-1">HOT</div>
+                                        <img src="./img/shoes-tosca.png" alt="">
+                                    </div>
+                                    <h1>Nike Netro</h1>
+                                    <div class="harga">
+                                        <p>$135.00</p>
+                                        <p><s>$289,00</s></p>
+                                    </div>
+                                    <div class="color">
+                                        <button>Colors</button>
+                                        <div class="radius">
+                                            <div class="radius-x radius-a"></div>
+                                            <div class="radius-x radius-b"></div>
+                                            <div class="radius-x radius-c"></div>
+                                            <div class="radius-x radius-d"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="product-4">
+                                    <div class="box">
+                                        <img src="./img/shoes-pelangi.png" alt="">
+                                    </div>
+                                    <h1>Nike Spain</h1>
+                                    <div class="harga">
+                                        <p>$149.00</p>
+                                        <p><s>$199,00</s></p>
+                                    </div>
+                                    <div class="color">
+                                        <button>Colors</button>
+                                        <div class="radius">
+                                            <div class="radius-x radius-a"></div>
+                                            <div class="radius-x radius-b"></div>
+                                            <div class="radius-x radius-c"></div>
+                                            <div class="radius-x radius-d"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="samping-kiri">
+                        <div class="atas">
+                            <div class="baris-atas">
+                                <i class="fa-regular fa-circle-user"></i>
+                            </div>
+                            <div class="baris2">
+                                <p>People reached</p>
+                                <i class="fa-solid fa-circle-info"></i>
+                            </div>
+                            <h1>256k</h1>
+                            <div class="baris-bawah">
+                                <i class="fa-solid fa-arrow-up"></i>
+                                <div class="persent">37.8%</div>
+                                <p>this week</p>
+                            </div>
+                        </div>
+                        <div class="bawah">
+                            <div class="baris-atas">
+                                <i class="fa-solid fa-arrows-up-down"></i>
+                            </div>
+                            <div class="baris2">
+                                <p>Engagement</p>
+                                <i class="fa-solid fa-circle-info"></i>
+                            </div>
+                            <h1>1.2x</h1>
+                            <div class="baris-bawah">
+                                <i class="fa-solid fa-arrow-up"></i>
+                                <div class="persent">37.8%</div>
+                                <p>this week</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </div>
+    </section>
+</body>
+
+</html>
